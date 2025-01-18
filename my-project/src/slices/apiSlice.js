65c6c931ed3bdbbf2744ebd84db7e71abd0a6a62
Arbazkhan04 <arbaz582021@gmail.com>
2025-01-18 13:43:52 -0500

    basypass wroks

--- my-project/src/slices/apiSlice.js
+++ my-project/src/slices/apiSlice.js
@@ -1,9 +1,9 @@
 import { fetchBaseQuery, createApi } from "@reduxjs/toolkit/query/react";
 
-// const baseURL =' https://searchflow-ed703fb051f2.herokuapp.com'
+const baseURL ='https://searchflow-ed703fb051f2.herokuapp.com'
 // const baseURL = "http://localhost:3003";
 
-const baseURL = "/api"; // No need to hard-code the backend URL
+// const baseURL = "/api"; // No need to hard-code the backend URL
 const baseQuery = fetchBaseQuery({ baseUrl: baseURL });
 
 export const apiSlice = createApi({
--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://searchflow-ed703fb051f2.herokuapp.com", // Your backend server URL
-        // target: "http://localhost:3003", // Your backend server URL
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
+  base: "/",
+  // server: {
+  //   proxy: {
+  //     "/api": {
+  //       target: "https://searchflow-ed703fb051f2.herokuapp.com", // Your backend server URL
+  //       // target: "http://localhost:3003", // Your backend server URL
+  //       changeOrigin: true,
+  //       rewrite: (path) => path.replace(/^\/api/, ""),
+  //     },
+  //   },
+  // },
 });
