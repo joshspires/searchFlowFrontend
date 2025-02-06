d7df5ebfe505986c79680d5d5cd29a01c0d0bc9d
Josh Spires <jspires111@gmail.com>
2025-02-06 21:48:04 +0800

    UPDATE

--- my-project/vite.config.js
+++ my-project/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   base: "/",
-  // server: {
-  //   proxy: {
-  //     "/api": {
-  //       target: "https://searchflow-ed703fb051f2.herokuapp.com", // Your backend server URL
-  //       // target: "http://localhost:3003", // Your backend server URL
-  //       changeOrigin: true,
-  //       rewrite: (path) => path.replace(/^\/api/, ""),
-  //     },
-  //   },
-  // },
+  server: {
+    proxy: {
+      "/api": {
+        target: "https://dash.searchflow.app", // Your Heroku backend domain
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
 });
